29b7deb86c6decf56c6eae7a2b931baecc85ef6e
ayyan zubair <ayyanzubair7@gmail.com>
2025-04-28 17:00:36 +0500

    remove public decorator

--- src/user-management/dashboard-users.controller.ts
+++ src/user-management/dashboard-users.controller.ts
@@ -10,7 +10,6 @@ import {
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { CreateUserCommand } from './commands/impl/create-user.command';
-import { Public } from 'src/auth/auth.guard';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UpdateUserCommand } from './commands/impl/update-user.command';
 import { DeleteUserCommand } from './commands/impl/delete-user.command';
@@ -24,31 +23,26 @@ export class DashboardUsersController {
     private readonly queryBus: QueryBus,
   ) {}
 
-  @Public()
   @Get('/get-all-users')
   async getAllUsers(): Promise<any> {
     return await this.queryBus.execute(new GetAllUsersQuery());
   }
 
-  @Public()
   @Get(':id')
   async getUserById(@Param('id') id: string): Promise<any> {
     return await this.queryBus.execute(new GetUserQuery(id));
   }
 
-  @Public()
   @Post('create-user')
   async createUser(@Body() data: CreateUserDto): Promise<any> {
     return await this.commandBus.execute(new CreateUserCommand(data));
   }
 
-  @Public()
   @Put('update-user')
   async updateUser(@Body() data: UpdateUserDto): Promise<any> {
     return await this.commandBus.execute(new UpdateUserCommand(data));
   }
 
-  @Public()
   @Delete(':id')
   async deleteUser(@Param('id') id: string): Promise<any> {
     return await this.commandBus.execute(new DeleteUserCommand(id));
