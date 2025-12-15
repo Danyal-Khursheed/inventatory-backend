1462cc5c4cf90aeb0b6d080735067c0e2041fafd
Danyal Khursheed <danyalkhursheed9@gmail.com>
2025-12-15 18:12:39 +0500

    added another api for update apckages

--- src/package/commands/handlers/index.ts
+++ src/package/commands/handlers/index.ts
@@ -1,7 +1,13 @@
 // commands/handlers/index.ts
+import { UpdatePackageCommand } from '../impl/update-package.command';
 import { CreatePackageHandler } from './create-package.handler';
 import { DeletePackageHandler } from './delete-package.handler';
+import { UpdatePackageHandler } from './update-package.handler';
 
-const commandHandlers = [CreatePackageHandler, DeletePackageHandler];
+const commandHandlers = [
+  CreatePackageHandler,
+  DeletePackageHandler,
+  UpdatePackageHandler,
+];
 
 export default commandHandlers;
